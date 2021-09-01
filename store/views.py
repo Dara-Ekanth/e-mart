b72b330650e6345dc35126be02fecd73e248ddc7
Dara-Ekanth <daraekanth3@gmail.com>
2021-09-01 08:49:03 +0530

    view for review is created and most of the corner cases is taken care.

--- store/views.py
+++ store/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import JsonResponse
 import json
 from django.contrib.auth.decorators import login_required
 from .models import *
 from .filters import *
+from .forms import *
+from django.contrib import messages
 
 
 # Create your views here.
@@ -28,7 +30,8 @@ def ordered_item(request):
 @login_required(login_url='login_page')
 def product_info(request, id):
     product = Product.objects.get(id=id)
-    context = {'product': product}
+    review = Review.objects.filter(product_id=id)
+    context = {'product': product,'review':review}
     print(product.name, product.price)
     return render(request, 'store/product_info.html', context=context)
 
@@ -102,3 +105,34 @@ def prev_items(request, id):
     prev_item = OrderItem.objects.filter(order_id=id)
     context = {'prev_item': prev_item}
     return render(request, 'store/prev_items.html', context)
+
+def review_items(request,id):
+    #This is to check if user is writing review before ordering
+    orders_by_the_customer = Order.objects.filter(customer=request.user,paid=False)
+    checking_in_order_items = OrderItem.objects.filter(product_id=id,order__in=orders_by_the_customer)
+    print(checking_in_order_items,"checking order items")
+    if len(checking_in_order_items)>=1:
+        message = "You didn't order yet or your payment is not yet completed."
+        messages.error(request,message)
+        return redirect('prev_orders')
+    current_form = review_form()
+    #For updating the review written
+    try:
+        update_review = Review.objects.get(product_id=id,customer=request.user)
+        print(update_review)
+        if update_review:
+            current_form = review_form(instance=update_review)
+    except Exception as e:
+        message = "Your review is Valuble for us"
+        messages.success(request,message)
+    if request.method == 'POST':
+        current_form = review_form(request.POST)
+        if current_form.is_valid():
+            Review.objects.create(customer=request.user,product_id=id,content=current_form.cleaned_data.get('content'))
+            return redirect('prev_orders')
+        else:
+            message = "Task Creation Failed."
+            messages.error(request, message)
+    context = {'form': current_form}
+
+    return render(request,'store/review_items.html',context)
